5a6e4fc7d8b088ab882da33a76dc91be947a1e4f
Yu-Hua Chang <yuhua@taiwankk.com>
2017-01-19 17:36:01 +0800

    fix aad login issue

--- src/main/java/com/example/SpringBootOauth2AzureadApplication.java
+++ src/main/java/com/example/SpringBootOauth2AzureadApplication.java
@@ -2,11 +2,46 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultRequestEnhancer;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
+import org.springframework.util.MultiValueMap;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SpringBootOauth2AzureadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOauth2AzureadApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2AzureadApplication.class, args);
+    }
+
+    @Bean
+    public UserInfoRestTemplateCustomizer getUserInfoRestTemplateCustomizer() {
+        return new UserInfoRestTemplateCustomizer() {
+            @Override
+            public void customize(OAuth2RestTemplate template) {
+                template.setAccessTokenProvider(new MyAuthorizationCodeAccessTokenProvider());
+            }
+        };
+    }
+    
+    protected class MyAuthorizationCodeAccessTokenProvider extends AuthorizationCodeAccessTokenProvider {
+        public MyAuthorizationCodeAccessTokenProvider() {
+            setTokenRequestEnhancer(new DefaultRequestEnhancer() {
+                @Override
+                public void enhance(AccessTokenRequest request, OAuth2ProtectedResourceDetails resource, MultiValueMap<String, String> form, HttpHeaders headers) {
+                    super.enhance(request, resource, form, headers);
+                    form.set("resource", "https://graph.windows.net/");
+                }
+            });
+        }
+    }
+
+
 }
